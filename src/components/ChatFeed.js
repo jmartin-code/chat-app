9e7cbe2659988cc6bebd3854aca02e5354b9299f
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2021-02-05 15:47:28 -0500

    structuring the chat window

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -13,11 +13,33 @@ export default function ChatFeed(props) {
     // console.log(keys);
     return keys.map((keys, index) => {
       const message = messages[keys];
-      const lastMessage = index === 0 ? null : keys[index - 1];
       const isPrivate = userName === message.sender.username;
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+
+      return (<div key={`msg_${index}`} style = {{width = "90%"}}>
+        <div className = "message-section">
+          {
+            isPrivate ? <privateMessage message = {Message}/> : <publicMessage lastMessage={messages[lastMessageKey]} message = {Message}/>
+          }
+        </div>
+        <div className = "read-check" style = {{marginRight: isPrivate ? "10px" : "0", marginLeft:isPrivate ? "0" : "60px" }}>
+          
+        </div>
+      </div>)
     });
   };
-  renderMessages();
 
-  return <div>Hola</div>;
+  if(!chat) return "Waiting for chat..."
+
+  return <div className = "chat">
+    <div className = "chat-container">
+      <div className= "chat-title">{chat.title}</div>
+      <div className= "chat-subtitle">{chat.people.map((user)=>`${user.person.username}`)}</div>
+    </div>
+    {renderMessages()}
+    <div style = {{height: "50px"}}/>
+    <div className="form">
+      <Form {...props} chatId={activeChat}/>
+    </div>
+  </div>;
 }
---
+++ src/components/publicMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function publicMessage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
